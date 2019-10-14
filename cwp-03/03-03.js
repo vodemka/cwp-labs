8edff41383dc7b589a92543bb72fa4e7dc29747e
Vodemka <vodemka@mail.ru>
2019-10-14 22:15:36 +0300

    Task 02 and 03 refactoring

--- cwp-03/03-03.js
+++ cwp-03/03-03.js
@@ -13,14 +13,19 @@ http.createServer(function (request, response) {
         if (typeof url.parse(request.url, true).query.k != "undefined") {
             let k = parseInt(url.parse(request.url, true).query.k);
             if (Number.isInteger(k)) {
-                response.writeHead(200, { "Content-Type": "application/json; charset=utf-8" });
-                response.end(JSON.stringify({ k: k, fact: fact(k) }));
+                //response.writeHead(200, { "Content-Type": "application/json; charset=utf-8" });
+                response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
+                response.end("<!DOCTYPE html>"+
+                    "<html>"+
+                    "<head><title>Document</title></head>"+
+                    "<body><h3>Влада милашка!!!</h3></body>"+
+                    "</html >");
             }
         }
     }
     else if (url.parse(request.url).pathname === "/") {
         let start_time = new Date().getTime();
-        var t = fs.readFileSync("03-03.html");
+        var t = fs.readFileSync("index.html");
         response.writeHead(200, { "Content-Type": "text/html; charset=utf-8" });
         response.end(t);
     }
